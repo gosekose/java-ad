6baab83aa6c477af0b00eb3282c6f4b48062cafb
gosekose <ksyn1611@naver.com>
2024-08-04 05:36:08 +0900

    자바 스레드 14: blockingQueue 적용

--- src/thread/bounded/BoundedMain.java
+++ src/thread/bounded/BoundedMain.java
@@ -13,11 +13,11 @@ public class BoundedMain {
     public static void main(String[] args) {
         // 1.  BoundedQueue 선택
 //        BoundedQueue queue = new BoundedQueueV1(2);
-        BoundedQueue queue = new BoundedQueueV5(2);
+        BoundedQueue queue = new BoundedQueueV6_1(2);
 
         // 2. 생산자, 소비자 실행 순서 선택, 반드시 하나만 선택
-        producerFirst(queue);
-//        consumerFirst(queue);
+//        producerFirst(queue);
+        consumerFirst(queue);
     }
 
     private static void consumerFirst(BoundedQueue queue) {
